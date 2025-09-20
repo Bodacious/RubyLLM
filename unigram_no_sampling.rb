3011656a0d561bf7587840ed2ea54498f484bedb
Gavin Morrice <gavin@gavinmorrice.com>
2025-09-20 19:23:16 +0100

    Refactored Unigram to be more readable

--- unigram_no_sampling.rb
+++ unigram_no_sampling.rb
@@ -11,6 +11,8 @@ counts = Hash.new(0)
 CORPUS.each { |tok| counts[tok] += 1 }
 total = counts.values.sum.to_f
 probs = counts.transform_values { |c| c / total }
-
-sample_text = MAX_TOKENS.times.map { |i| probs.sort_by { |(token_a, prob_a), (token_b, prob_b)| prob_b <=> prob_a }.first.first }.join(" ")
+generate_next_token = Proc.new do |(_token_a, prob_a), (_token_b, prob_b)|
+  probs.sort_by { prob_b <=> prob_a }.first.first
+end
+sample_text = MAX_TOKENS.times.map(&generate_next_token).join(" ")
 puts sample_text
