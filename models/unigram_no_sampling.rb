6b476a7c66a542a3830e034017edf0493e60652b
Gavin Morrice <gavin@gavinmorrice.com>
2025-09-20 19:50:33 +0100

    Refactor Unigram no sampling
    Added tests to make sure it's working

--- models/unigram_no_sampling.rb
+++ models/unigram_no_sampling.rb
@@ -4,15 +4,17 @@ require 'bundler'
 
 Bundler.setup(:development)
 
-CORPUS = "the cat sat on the mat".split
-MAX_TOKENS = 10
-
-counts = Hash.new(0)
-CORPUS.each { |tok| counts[tok] += 1 }
-total = counts.values.sum.to_f
-probs = counts.transform_values { |c| c / total }
-generate_next_token = Proc.new do |(_token_a, prob_a), (_token_b, prob_b)|
-  probs.sort_by { prob_b <=> prob_a }.first.first
+class UnigramNoSampling
+  CORPUS = "the cat sat on the mat".split
+  MAX_TOKENS = 10
+  def generate(sequence_length = MAX_TOKENS)
+    counts = Hash.new(0)
+    CORPUS.each { |tok| counts[tok] += 1 }
+    total = counts.values.sum.to_f
+    probs = counts.transform_values { |c| c / total }
+    generate_next_token = Proc.new do |(_token_a, prob_a), (_token_b, prob_b)|
+      probs.sort_by { prob_b <=> prob_a }.first.first
+    end
+    sequence_length.times.map(&generate_next_token).join(" ")
+  end
 end
-sample_text = MAX_TOKENS.times.map(&generate_next_token).join(" ")
-puts sample_text
---
+++ test/models/unigram_no_sampling_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+require_relative '../../models/unigram_no_sampling'
+
+class UnigramNoSamplingTest < Minitest::Test
+  def test_generate_returns_the_n_times
+    model = UnigramNoSampling.new
+
+    sample_text = model.generate(10)
+
+    assert_equal "the the the the the the the the the the", sample_text
+  end
+end
