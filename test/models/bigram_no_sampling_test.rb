905b155fb2dbe5dd123f3042aed8a8f3143d8012
Gavin Morrice <gavin@gavinmorrice.com>
2025-09-21 13:08:32 +0100

    Add more comprehensive tests for bigram model

--- test/models/bigram_no_sampling_test.rb
+++ test/models/bigram_no_sampling_test.rb
@@ -2,11 +2,37 @@ require 'test_helper'
 require_relative '../../models/bigram_no_sampling'
 
 class BigramNoSamplingTest < Minitest::Test
-  def test_generate_returns_the_expected_sentence_with_correct_prompt
+
+  def test_generate_returns_only_prompt_string_when_prompt_not_in_corpus
     model = BigramNoSampling.new
 
-    sample_text = model.generate(prompt: "the", sequence_length: 5)
+    sample_text = model.generate(prompt: "foo", sequence_length: 5)
 
-    assert_equal "the cat sat on the mat", sample_text
+    assert_equal "foo", sample_text
+  end
+
+  def test_generate_returns_content_when_word_in_corpus
+    model = BigramNoSampling.new
+
+    sample_text = model.generate(prompt: "cat", sequence_length: 5)
+
+    assert_equal "cat sat on the cat", sample_text
+  end
+
+  def test_generate_returns_the_correct_sequence_length
+    model = BigramNoSampling.new
+
+    sample_text = model.generate(prompt: "cat", sequence_length: 10)
+
+    assert_equal 10, sample_text.split.length
+  end
+
+  def test_generate_returns_most_probable_output_with_correct_start_prompt
+    model = BigramNoSampling.new
+
+    sample_text = model.generate(prompt: "the", sequence_length: 6)
+
+    # This is ending in "cat" not "mat"
+    assert_equal "the cat sat on the cat", sample_text
   end
 end
