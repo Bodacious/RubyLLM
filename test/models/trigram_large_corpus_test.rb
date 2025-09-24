e1db10191da7595f9dc5a6bcd121826325de289f
Gavin Morrice <gavin@gavinmorrice.com>
2025-09-24 21:34:16 +0100

    Remove legal messages from corpus text

--- test/models/trigram_large_corpus_test.rb
+++ test/models/trigram_large_corpus_test.rb
@@ -18,11 +18,11 @@ class TrigramLargeCorpusTest < Minitest::Test
 
     sample_text = model.generate(prompt: "These", sequence_length: 100)
 
-    assert_equal "these motives urged me forward;\
- i seemed to forget those friends who were so many misfortunes weigh upon you,\
- but to adapt my nature had the shape of a library of electronic works, by\
- using or distributing this work i obtained from my infancy i was nourished\
- with high hopes and joys.", sample_text
+    assert_equal <<~STRING.chomp, sample_text
+      these motives urged me forward; i seemed to forget those friends who \
+      were so many misfortunes weigh upon you, but to adapt my nature had the \
+      shape of a mummy.
+    STRING
     refute_empty sample_text
   end
 
@@ -32,11 +32,7 @@ class TrigramLargeCorpusTest < Minitest::Test
     sample_text = model.generate(prompt: "and when I left", sequence_length: 100)
 
     # THIS IS COMPLETELY GENERATED CONTENT!
-    assert_equal "night also closed around; and when wrenched by misery\
- without feeling the most verdant islands that relieve the eye and in you for\
- the limited right of replacement or refund described in these works, so full\
- of lofty design and heroism, this elevation of mind preyed upon my hands,\
- as a rock.", sample_text
+    assert_equal "and when i left him incredulous to the vessel.", sample_text
     refute_empty sample_text
   end
 end
