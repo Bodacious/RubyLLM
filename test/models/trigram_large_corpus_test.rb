5dc9af42107098391f41bd2b90cbca50b3c4a21e
Gavin Morrice <gavin@gavinmorrice.com>
2025-09-28 14:17:56 +0100

    Trigram with Tokenized corpus working

--- test/models/trigram_large_corpus_test.rb
+++ test/models/trigram_large_corpus_test.rb
@@ -9,30 +9,31 @@ class TrigramLargeCorpusTest < Minitest::Test
 
     sample_text = model.generate(prompt: "I cannot pretend to describe")
 
-    assert_equal "i am alone.", sample_text
+    assert_equal "i cannot pretend to describe what i have no doubt of your own.", sample_text
     refute_empty sample_text
   end
 
-  def test_returns_sensible_content_when_prompt_is_These
+  def test_returns_sensible_content_when_prompt_is_a_few_common_words
     model = TrigramLargeCorpus.new
 
-    sample_text = model.generate(prompt: "I don't want to")
+    sample_text = model.generate(prompt: "and my first of those")
 
     assert_equal <<~STRING.chomp, sample_text
-      these motives urged me forward; i seemed to forget those friends who \
-      were so many misfortunes weigh upon you, but to adapt my nature had the \
-      shape of a mummy.
+      and my first of those whose dearest victor, do not know the names of the \
+      lake, which was to be the consolation of your own.
     STRING
     refute_empty sample_text
   end
 
-  def test_returns_sensible_content_when_prompt_is_night
+  def test_gets_caught_in_loops_on_common_phases
     model = TrigramLargeCorpus.new
 
-    sample_text = model.generate(prompt: "and when I left")
+    sample_text = model.generate(prompt: "but i was")
 
     # THIS IS COMPLETELY GENERATED CONTENT!
-    assert_equal "and when i left him incredulous to the vessel.", sample_text
+    assert_includes sample_text, "but i was unable to solve these questions,\
+ but i was unable to solve these questions,\
+ but i was unable to solve these questions"
     refute_empty sample_text
   end
 end
