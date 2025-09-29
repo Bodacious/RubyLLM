0434e55749a6729c60453fc4b754b85bf4fa73d5
Gavin Morrice <gavin@gavinmorrice.com>
2025-09-29 23:00:28 +0100

    Add temperature to make random variance scalable

--- lib/conditional_distribution.rb
+++ lib/conditional_distribution.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class ConditionalDistribution
-  def initialize(context:, token_probs:)
+  def initialize(context:, token_probs:, temperature: ENV.fetch('TEMPERATURE', 1.0))
     @context = context
     @token_probs = token_probs # { NGram => Float (probability) }
+    @temperature = temperature.to_f
+    raise ArgumentError, "temperature must be > 0" unless @temperature.positive?
+
+    apply_temperature_scaling!
   end
 
   attr_reader :context
@@ -18,15 +22,21 @@ class ConditionalDistribution
     @token_probs.keys.last
   end
 
-  def [](token)
-    @token_probs[token]
-  end
-
   def tokens
     @token_probs.keys
   end
 
-  def probabilities
-    @token_probs.values
+  private
+
+  def apply_temperature_scaling!
+    logits = @token_probs.values.map { |probability| Math.log(probability) }
+    scaled = logits.map { |logit| Math.exp(Rational(logit, @temperature)) }
+
+    total = scaled.sum
+    normalised = scaled.map { |val| Rational(val, total) }
+
+    @token_probs.zip(normalised).map do |(ngram, _p), new_probability|
+      @token_probs[ngram] = new_probability
+    end
   end
 end
