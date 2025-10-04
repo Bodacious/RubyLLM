ee83f38feac7edf1b4c3be3f81cb36650e96123c
Gavin Morrice <gavin@gavinmorrice.com>
2025-10-04 13:52:57 +0100

    Extract classes for readability

--- models/language_model.rb
+++ models/language_model.rb
@@ -1,3 +1,59 @@
+TokenProbability = Data.define(:token, :probability)
+class Tokenizer
+  def initialize(document:)
+    @tokens = document.to_s.split
+  end
+  def tokens
+    @tokens
+  end
+end
+
+class ProbabilityDistribution
+  def initialize(token_counts = {})
+    @token_counts = token_counts
+  end
+
+  def [](context)
+    distribution.fetch(context, [])
+  end
+  def distribution
+    return @distribution if defined?(@distribution)
+
+    @distribution = @token_counts.map do |context, target_counts|
+      total_token_count = target_counts.values.sum
+      target_probabilities = target_counts.map { |target, count|
+        probability = count / total_token_count.to_f
+        TokenProbability[target, probability]
+      }
+
+      [context, target_probabilities]
+    end.to_h
+  end
+end
+class NGramCounter
+  def initialize(tokens, n: 2)
+    @tokens = tokens
+    @n = n
+    # Each key in this hash defaults to a value of a new hash with default 0
+    #   Example:
+    #     counts['foo']['bar'] # => 0
+    #     counts # => { "foo" => { "bar" => 0 }}
+    @counter = Hash.new { |h, k| h[k] = Hash.new(0) }
+    count_n_grams
+  end
+
+  def token_counts
+    @counter
+  end
+
+  protected
+  def count_n_grams
+    @tokens.each_cons(@n) do |(t1, t2)|
+      @counter[t1][t2] += 1
+    end
+    @counter
+  end
+end
 
 class LanguageModel
   DOCUMENT = 'the cat sat on the mat'
@@ -17,35 +73,13 @@ class LanguageModel
 
   protected
 
-  TokenProbability = Data.define(:token, :probability)
-  class Tokenizer
-    def initialize(document:)
-      @tokens = document.to_s.split
-    end
-
-    def each_cons(...)
-      @tokens.each_cons(...)
-    end
-  end
   def generate_next_token(context:)
-    puts @probability_distributions.inspect
     @probability_distributions[context].max_by(&:probability).token
   end
 
   def calculate_probability_distributions
-    token_counts = Hash.new { |h, k| h[k] = Hash.new { |h2, k2| h2[k2] = 0 } }
-    Tokenizer.new(document: DOCUMENT).each_cons(2) do |(t1, t2)|
-      token_counts[t1][t2] += 1
-    end
-
-    token_counts.map do |context, target_counts|
-      total_token_count = target_counts.values.sum
-      target_probabilities = target_counts.map { |target, count|
-        probability = count / total_token_count.to_f
-        TokenProbability[target, probability]
-      }
-
-      [context, target_probabilities]
-    end.to_h
+    tokens = Tokenizer.new(document: DOCUMENT).tokens
+    token_counts = NGramCounter.new(tokens).token_counts
+    ProbabilityDistribution.new(token_counts)
   end
 end
